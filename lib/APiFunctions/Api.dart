ed5d59f38d78839e2bccd22fbc6e69853592bdf5
hussein samir <hsamir@aramwest.com>
2021-03-20 15:36:34 +0200

    adding example apis

--- lib/APiFunctions/Api.dart
+++ lib/APiFunctions/Api.dart
@@ -1,3 +1,75 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class Api {}
+import 'package:flutter/material.dart';
+import 'package:gas/utils/toast.dart';
+import 'package:http/http.dart'as http;
+import 'package:xs_progress_hud/xs_progress_hud.dart';
+
+class Api {BuildContext context;
+GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
+Api(this.context,this.scaffoldKey);
+
+Future<dynamic> getProducts() async {
+String url="";
+String getProducts="";
+
+   // TODO: implement getStudents
+   final response = await http.get(
+    "${url}$getProducts",
+
+    // body: {
+    //   "ngo_id" : "${StaticVars.Base_NgoId}"
+    // },
+    headers: {
+      HttpHeaders.authorizationHeader : '',
+    },
+  );
+  print("ResponseInfo ${json.decode(response.body)}");
+  XsProgressHud.hide();
+
+  if(response.statusCode == 200){
+    // return EventDetailsModel.fromJson(json.decode(response.body));
+    // return EventDetailsModel.fromJson(json.decode(response.body));
+
+  }else{
+    FN_showToast('Error', context, Colors.red,scaffoldKey);
+    return false;
+  }
+
+
+}
+Future loginFunc(String password,)async{
+  String url = "";
+    //TklwOrrqCqzmwLVHYFZslvdgWU660xPZLUEjRtOvTMmcOG2UH41TX4PiDkgo
+  var response = await http.post(
+      url,
+      body: {
+        "password" : password
+      },
+      headers: {
+        HttpHeaders.authorizationHeader : 'Bearer ',
+      }
+  );
+  print("PhoneBody ${response.body}");
+  print("PhoneBody ${response.statusCode}");
+  if(response.statusCode == 200){
+    if(!response.body.contains("error")){
+      FN_showToast('Successfully updated', context, Colors.green,scaffoldKey);
+
+      return true;
+      // if(response.statusCode == 200){
+
+    }else{
+
+      FN_showToast('${json.decode(response.body)['error']}', context, Colors.red,scaffoldKey);
+      return false;
+    }
+  }else{
+    FN_showToast('Send Code Failed', context, Colors.red,scaffoldKey);
+    return false;
+  }
+}
+
+}
