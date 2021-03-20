28fc45ee365277711678aba02ce588bc726958ec
nader <nadersalah20201111@gmail.com>
2021-03-20 15:21:25 +0200

    edits

--- lib/utils/custom_widgets/custom_button.dart
+++ lib/utils/custom_widgets/custom_button.dart
@@ -3,39 +3,33 @@ import 'package:gas/utils/colors_file.dart';
 
 class CustomButton extends StatelessWidget {
   final double bttnHeight;
-  final double bttnWidth;
   final String bttnName;
   final double bttnNameSize;
   final Function onPress;
 
   CustomButton(
-      { this.bttnHeight,
-       this.bttnWidth,
-       this.bttnName,
-       this.bttnNameSize,
-       this.onPress});
+      {this.bttnHeight, this.bttnName, this.bttnNameSize, this.onPress});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        alignment: Alignment.center,
-        child: ButtonTheme(
-            minWidth: bttnWidth,
-            height: bttnHeight,
-            child: RaisedButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              color: primaryAppColor,
-              child: Text(
-                bttnName,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: bttnNameSize,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-              onPressed: () => onPress,
-            )));
+        width: MediaQuery.of(context).size.width,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            primary: primaryAppColor,
+            onPrimary: Colors.white,
+          ),
+          child: Text(
+            bttnName,
+            style: TextStyle(
+              fontSize: bttnNameSize,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          onPressed: onPress,
+        ));
   }
 }
